12eea9d8e3a3a16f4fcd3e1b1f170ba7ababc6ed
jklein <jklein@71ff03b8-2c0e-0410-b06c-f0d72565b0c3>
2004-08-20 13:38:10 +0000

    *** empty log message ***

---
+++ kernel/internalFunctions/breveFunctionsStationary.cc
@@ -0,0 +1,55 @@
+/*****************************************************************************
+ *                                                                           *
+ * The breve Simulation Environment                                          *
+ * Copyright (C) 2000, 2001, 2002, 2003 Jonathan Klein                       *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with this program; if not, write to the Free Software               *
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA *
+ *****************************************************************************/
+
+#include "kernel.h"
+
+/*! \addtogroup InternalFunctions */
+/*@{*/
+
+int brIAddStationary(brEval *args, brEval *target, brInstance *i) {
+	slShape *sh = BRPOINTER(&args[0]);
+	slVector *v = &BRVECTOR(&args[1]);
+	slStationary *st;
+	slWorldObject *wo;
+
+	double id[3][3];
+
+	if(!sh) {
+		slMessage(DEBUG_ALL, "null pointer passed to addStationary\n");
+		return EC_ERROR;
+	}
+
+	slMatrixIdentity(id);
+
+	st = slNewStationary(sh, v, id);
+
+	wo = slWorldAddObject(i->engine->world, st, WO_STATIONARY);
+	wo->userData = i;
+
+	BRPOINTER(target) = wo;
+   
+	return EC_OK;
+}
+
+/*@}*/
+
+void breveInitStationaryFunctions(brNamespace *n) {
+	brNewBreveCall(n, "addStationary", brIAddStationary, AT_POINTER, AT_POINTER, AT_VECTOR, 0);
+}
