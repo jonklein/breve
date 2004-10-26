fc7b48fb41d6e01256cc962624d1b05ce767f698
jklein <jklein@71ff03b8-2c0e-0410-b06c-f0d72565b0c3>
2004-10-26 14:28:06 +0000

    *** empty log message ***

---
+++ kernel/internalFunctions/breveFunctionsSpring.cc
@@ -0,0 +1,34 @@
+#include "kernel.h"
+
+int brISpringNew(brEval args[], brEval *target, brInstance *i) {
+	BRPOINTER(target) = slSpringNew(BRPOINTER(&args[0]), BRPOINTER(&args[1]), &BRVECTOR(&args[2]), &BRVECTOR(&args[3]), BRDOUBLE(&args[4]), BRDOUBLE(&args[5]), BRDOUBLE(&args[6]));
+
+	slWorldAddSpring(i->engine->world, BRPOINTER(target));
+
+	return EC_OK;
+}
+
+int brISpringSetStrength(brEval args[], brEval *target, brInstance *i) {
+	slSpring *spring = BRPOINTER(&args[0]);
+	spring->strength = BRDOUBLE(&args[1]);
+	return EC_OK;
+}
+
+int brISpringSetLength(brEval args[], brEval *target, brInstance *i) {
+	slSpring *spring = BRPOINTER(&args[0]);
+	spring->length = BRDOUBLE(&args[1]);
+	return EC_OK;
+}
+
+int brISpringSetDamping(brEval args[], brEval *target, brInstance *i) {
+	slSpring *spring = BRPOINTER(&args[0]);
+	spring->damping = BRDOUBLE(&args[1]);
+	return EC_OK;
+}
+
+void breveInitSpringFunctions(brNamespace *n) {
+    brNewBreveCall(n, "springNew", brISpringNew, AT_POINTER, AT_POINTER, AT_POINTER, AT_VECTOR, AT_VECTOR, AT_DOUBLE, AT_DOUBLE, AT_DOUBLE, 0);
+    brNewBreveCall(n, "springSetLength", brISpringSetLength, AT_NULL, AT_POINTER, AT_DOUBLE, 0);
+    brNewBreveCall(n, "springSetDamping", brISpringSetDamping, AT_NULL, AT_POINTER, AT_DOUBLE, 0);
+    brNewBreveCall(n, "springSetStrength", brISpringSetStrength, AT_NULL, AT_POINTER, AT_DOUBLE, 0);
+}
