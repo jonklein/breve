ea245710bb3851241a7c3accd85f52a40e24400a
jklein <jklein@71ff03b8-2c0e-0410-b06c-f0d72565b0c3>
2004-11-30 12:31:37 +0000

    *** empty log message ***

--- simulation/lightdetector.h
+++ simulation/lightdetector.h
@@ -18,4 +18,10 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA *
  *****************************************************************************/
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 void slDetectLightExposure(slWorld *w, slCamera *c, int size, GLubyte *buffer);
+#ifdef __cplusplus
+}
+#endif
