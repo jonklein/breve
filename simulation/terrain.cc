e4a4abe6bc44d33dd8f24e97a0f1c3f575add2bb
ianlindsay <ianlindsay@71ff03b8-2c0e-0410-b06c-f0d72565b0c3>
2005-01-13 00:30:41 +0000

    simulation.h includes this for us.

--- simulation/terrain.cc
+++ simulation/terrain.cc
@@ -18,7 +18,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA *
  *****************************************************************************/
 
-#include <limits>
 #include "simulation.h"
 
 #ifdef HAVE_LIBTIFF
