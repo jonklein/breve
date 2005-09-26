80a8bef727d6043dd20a459a1a295bacfc1ade00
eejd <eejd@71ff03b8-2c0e-0410-b06c-f0d72565b0c3>
2005-09-26 23:57:32 +0000

    initialize all vectors and matricies to 0.0.

--- neural/Network/neuralNetwork.cc
+++ neural/Network/neuralNetwork.cc
@@ -61,6 +61,10 @@ slNeuralNetwork::slNeuralNetwork(const unsigned size)
 {
     _inputNeuronStates = 0;
     _outputNeuronStates = 0;
+    _previousNeuronStates.setAll(0.0);
+    _currentNeuronStates.setAll(0.0);
+    _connectionWeights.setAll(0.0);
+    _connectionStructure.setAll(0.0);
 }
 
 void slNeuralNetwork::setNeuronTimeStep(const float scalar)
